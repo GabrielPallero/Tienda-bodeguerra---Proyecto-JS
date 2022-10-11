e101ba02bc21c13ed88a32ab055c288c122c8cf0
Gabriel Pallero <pallerogabriel93@gmail.com>
2022-10-11 08:52:31 -0300

    Agregando eleccion y funcion para votacion

--- preentrega1/preentrega1.js
+++ preentrega1/preentrega1.js
@@ -19,3 +19,16 @@ else if(usuario != "Gabriel" || contraseña != 1921){
 }
     
 }
+let eleccion = prompt("SELECCIONA UNA DE LAS 4 CAMISETAS");
+if (eleccion == 1) {
+    alert("ELEGISTE TEMPORADA 2011-2012");
+}else if (eleccion == 2) {
+    alert("ELEGISTE TEMPORTADA 2016-2017");
+}else if (eleccion == 3) {
+    alert("ELEGISTE TEMPORADA 2018");
+}else if (eleccion == 4) {
+    alert("ELEGISTE TEMPORTADA 2022");
+}
+else {
+    alert("NO SE SELLECIONO NINGUNA OPCIÓN"); 
+}
